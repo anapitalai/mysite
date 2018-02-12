3c62329a1650b83de2dc20c62bddbab1fc4331cb
anapitalai <anapitalai@gmail.com>
2018-02-12 17:50:23 +1000

    Second commit,addition of the landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,92 +4,84 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat|Quicksand" rel="stylesheet">
+        
+        <script defer src="https://use.fontawesome.com/releases/v5.0.0/js/all.js"></script>
 
-        <title>Laravel</title>
+        <title>Napitalai ICT Consultants</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+  
+  <body>
+    <div id="wallpaper" class="wallpaper" data-image="images/austria.jpg"></div>
+    <div class="content">
+    <aside class="side">
+    <figure id="picture" class="picture">
+      <div class="picture-shadow"></div>
+      <img id="pictureImage" class="picture-image"
+           src="images/sly.jpg"
+           srcset="images/sly.jpg 1x,
+                   images/sly.jpg 2x,
+                   images/sly.jpg 3x
+                   image/sly.jpg 4x"
+           alt="Portrait of Alois Napitalai"
+           width="320"
+           height="320">
+    </figure>
+  </aside>
+      <main class="about">
+        <h1 class="name">
+          Hi there, I am Alois Napitalai
+        </h1>
+        <p class="job">
+          Self Taught Web Developer
+        </p>
+        <hr class="hr">
+        <div class="description">
+          <p>
+             In learning,I believe in doing than talking and doing nothing!
+          </p>
         </div>
-    </body>
+        <div class="contact">
+          <a class="button" href="mailto:anapitalai@example.com">
+            Get in touch
+          </a>
+        </div>
+        <ul class="social">
+          <li>
+            <a href="https://twitter.com/alois.napitalai">
+              <i class="fab fa-twitter"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/anapitalai">
+              <i class="fab fa-github"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/">
+              <i class="fab fa-linkedin"></i>
+            </a>
+          </li>
+        </ul>
+      </main>
+    </div>
+    <noscript>
+      <style type="text/css">
+        .wallpaper {
+          animation-name: zoomOut;
+        }
+
+        .picture {
+          animation-name: dropIn;
+        }
+      </style>
+    </noscript>
+    <script src="{{ asset('js/app.js') }}"></script>
+
+  </body>
 </html>
+
